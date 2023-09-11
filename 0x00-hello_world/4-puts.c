2e557255d0b138c4d23334b93e95403f21b7adb6
omar <omarosa2003@gmail.com>
2023-09-11 15:16:45 -0700

    my first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Describtion: A C program that prints with put function
+ *
+ * Return: Always 0 (success)
+ */
+
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
