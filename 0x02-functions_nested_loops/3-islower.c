06eefbb49b591198ad13d758c3552acc9ab61e22
omar <omarosa2003@gmail.com>
2023-09-12 11:45:40 -0700

    finished

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,12 +7,12 @@
  * @c: checks input of function
  *
  * Return: returns 1 if `c` is lowercase
- * 	   otherwise always 0 (Success)
+ *	   otherwise always 0 (Success)
 */
 
 int _islower(int c)
 {
-	if (c >= 97 && c<= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	return (0);
 }
